5d01291544527688c170e0d29f62b58963b32233
azimbek01 <azimb3k@yandex.ru>
2020-06-25 15:56:16 +0600

    modified homework.py

--- homework.py
+++ homework.py
@@ -18,25 +18,25 @@ URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
-    if homework_name is None:
-        raise RuntimeError('Имя задания неопределено - None')
+    # if homework_name is None:
+    #     raise RuntimeError('Имя задания неопределено - None')
+    # else:
+    #     if homework.get('status') is None:
+    #         raise RuntimeError(
+    #             f'Неопределенный статус у задания {homework_name}'
+    #         )
+    #     else:
+    if homework['status'] == 'rejected':
+        verdict = 'К сожалению в работе нашлись ошибки.'
+    elif homework['status'] == 'approved':
+        verdict = 'Ревьюеру всё понравилось, можно ' \
+                    'приступать к следующему уроку.'
     else:
-        if homework.get('status') is None:
-            raise RuntimeError(
-                f'Неопределенный статус у задания {homework_name}'
-            )
-        else:
-            if homework['status'] == 'rejected':
-                verdict = 'К сожалению в работе нашлись ошибки.'
-            elif homework['status'] == 'approved':
-                verdict = 'Ревьюеру всё понравилось, можно ' \
-                            'приступать к следующему уроку.'
-            else:
-                raise RuntimeError(
-                    f'Неизвестный статус у задания {homework_name}'
-                )
+        raise RuntimeError(
+            f'Неизвестный статус у задания {homework_name}'
+        )
 
-            return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
